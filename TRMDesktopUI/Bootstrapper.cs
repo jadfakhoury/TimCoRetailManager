a7864a79d40b0e04486bfd46fbd620a55a82c351
Jad Fakhoury <jadfakhoury@hotmail.com>
2022-01-13 21:09:09 +0200

    login form created by not wired yet

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using TRMDesktopUI.ViewModels;
+using TRMDesktopUI.Helpers;
 
 namespace TRMDesktopUI
 {
@@ -16,6 +18,10 @@ namespace TRMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password", "PasswordChanged");
         }
 
         protected override void Configure()
